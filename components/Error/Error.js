f572a7e6d9df154735d63cd08c12a48f57619f12
Priya2422 <priyapandey.2415@gmail.com>
2023-12-29 12:51:33 +0530

    phirse try

--- components/Error/Error.js
+++
@@ -1,7 +0,0 @@
-import classes from './Error.module.css';
-export default function Error(props){
-    return <div className={classes.error}>
-        <p>{props.code}</p>
-        <p>{props.message}</p>
-    </div>
-}
--- pages/fav/index.js
+++ pages/fav/index.js
@@ -5,8 +5,8 @@ import Head from "next/head";
 
  function FavPage() {
   const {user}=useAuth();
-  if(user===false)return <>Auth Loading...</>
-  if(user){
+ 
+   if(user){
     return <>
     <Head>
       <title>Favorites</title>
@@ -14,13 +14,7 @@ import Head from "next/head";
     </Head>
      <Fav/>
     </>
-  }
-  return <>
-  <Head>
-      <title>Error</title>
-      <metadata name="description" content="Error occured"/>
-    </Head>
-  <Error code={404} message={'The page is not found'} />
-  </>
+   }
+  return <>Auth Loading...</>
 }
 export default FavPage;
